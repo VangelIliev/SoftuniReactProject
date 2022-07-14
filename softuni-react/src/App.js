81e18fe8e59dd99581e40cbca982ac41f0a917e4
Vangel <venonomer7@abv.bg>
2022-07-14 23:45:28 +0300

    Adding footer

--- softuni-react/src/App.js
+++ softuni-react/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header/header'
+import Footer from './components/footer/footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ softuni-react/src/components/footer/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <div className="row col-12 d-flex justify-content-center text-white">
+        <h3>Footer</h3>
+    </div>
+    )
+}
+export default Footer;
