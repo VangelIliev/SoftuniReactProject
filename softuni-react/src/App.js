b15df68ae42da64250d7ac65dc3c3ba490a04f20
Vangel <venonomer7@abv.bg>
2022-07-23 17:56:25 +0300

    Implementing routing

--- softuni-react/src/App.js
+++ softuni-react/src/App.js
@@ -2,15 +2,17 @@ import Header from './components/header/header'
 import Footer from './components/footer/footer'
 import Register from './components/register/register'
 import LogIn  from './components/login/login';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <LogIn/>
-      <Register/>
-      <Footer/>
+      <Routes>
+        <Route path='Register' element={<Register/>}/>
+        <Route path='Login' element={<LogIn/>}/>
+      </Routes>
     </div>
   );
 }
--- softuni-react/src/components/header/header.js
+++ softuni-react/src/components/header/header.js
@@ -4,13 +4,13 @@ function Header() {
         <nav className="bg-dark navbar-dark navbar">
             <div className="row col-12 d-flex justify-content-center text-white">
                 <ul className={styles.navigation}>
-                    <li className={styles.item}><a className={styles.link} href="#home">Home</a></li>
+                    <li className={styles.item}><a className={styles.link} href="home">Home</a></li>
                     <li className={styles.item}><a className={styles.link}>Recipes</a></li>
                     <li className={styles.item}><a className={styles.link}>My Recipes</a></li>
                     <li className={styles.item}><a className={styles.link}>About</a></li>
-                    <li className={styles.item}><a className={styles.link}>Login</a></li>
+                    <li className={styles.item}><a className={styles.link} href="Login">Login</a></li>
                     <li className={styles.item}><a className={styles.link}>LogOut</a></li>
-                    <li className={styles.item}><a className={styles.link}>Register</a></li>
+                    <li className={styles.item}><a className={styles.link} href="Register">Register</a></li>
                 </ul>
             </div>
         </nav>
