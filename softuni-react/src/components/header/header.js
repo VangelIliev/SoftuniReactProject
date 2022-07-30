eae0978ffcad4419f73c5f8b3bce77d30d5a592a
Vangel <venonomer7@abv.bg>
2022-07-30 13:06:41 +0300

    Adding links

--- softuni-react/src/components/header/header.js
+++ softuni-react/src/components/header/header.js
@@ -1,6 +1,7 @@
 import styles from './Header.module.css';
 import { signOut } from "firebase/auth";
 import { auth } from '../firebaseConfig.js';
+import { Link } from 'react-router-dom';
 function Header(props) {
     const logout = async () => {
         await signOut(auth);
@@ -10,11 +11,11 @@ function Header(props) {
             <nav className="bg-dark navbar-dark navbar">
             <div className="row col-12 d-flex justify-content-center text-white">
                 <ul className={styles.navigation}>
-                    <li className={styles.item}><a className={styles.link} href="Home">Home</a></li>
-                    <li className={styles.item}><a className={styles.link} href="Recipes">Recipes</a></li>
-                    <li className={styles.item}><a className={styles.link} href="MyRecipes">My Recipes</a></li>
-                    <li className={styles.item}><a className={styles.link} href="AddRecipe">Add Recipe</a></li>
-                    <li className={styles.item}><a className={styles.link} href="About">About</a></li>
+                    <li className={styles.item}><Link className={styles.link} to="/Home">Home</Link></li>
+                    <li className={styles.item}><Link className={styles.link} to="Recipes">Recipes</Link></li>
+                    <li className={styles.item}><Link className={styles.link} to="MyRecipes">My Recipes</Link></li>
+                    <li className={styles.item}><Link className={styles.link} href="AddRecipe">Add Recipe</Link></li>
+                    <li className={styles.item}><Link className={styles.link} href="About">About</Link></li>
                     <button className={styles.userNavButton} onClick={logout}>Log Out</button>             
                     <button className={styles.userNavButton}>Welcome {props.user}</button>                        
                 </ul>
