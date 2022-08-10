080631d954db6b4cc0815c3ce30d73e6530705dd
Vangel <venonomer7@abv.bg>
2022-08-11 00:16:50 +0300

    implementing delete

--- softuni-react/src/components/recipeDetails/recipeDetails.js
+++ softuni-react/src/components/recipeDetails/recipeDetails.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { doc,getDoc } from "firebase/firestore";
+import { doc,getDoc, deleteDoc } from "firebase/firestore";
 import { db } from '../firebaseConfig';
 import styles from './RecipeDetails.module.css';
+import { useNavigate } from "react-router-dom";
 function RecipeDetails(){
     const [recipe, setRecipe] = useState({});
     const params = useParams();
     const recipeId = params.recipeId;
-    
+    const navigate = useNavigate();
+
     async function getRecipe(recipeId) {
         try {
             const docRef = doc(db, "recipes", recipeId);
@@ -21,6 +23,18 @@ function RecipeDetails(){
         } catch (error) {
             alert(error);
         }
+    }
+    async function DeleteRecipe(){
+        try {
+            const docRef = doc(db, "recipes", recipeId);
+            await deleteDoc(docRef);
+            navigate("/MyRecipes", { replace: true });   
+        } catch (error) {
+            alert(error);
+        }
+    }
+    function EditRecipe(){
+        alert("iskam da napravq edit po tazi recepta");
     }
     useEffect( () => {
         getRecipe(recipeId);    
@@ -37,8 +51,8 @@ function RecipeDetails(){
                     <p>Servings: <strong>{recipe.servings}</strong></p>
                     <p>Preparation Time: <strong>{recipe.timeToPrepare} minutes</strong></p>
                     <p>Description: <strong>{recipe.description}</strong></p>
-                    <button className={styles.buttonRed}>Delete</button>
-                    <button className={styles.buttonBlue}>Edit</button>
+                    <button className={styles.buttonRed} onClick={DeleteRecipe}>Delete</button>
+                    <button className={styles.buttonBlue} onClick={EditRecipe}>Edit</button>
                 </div>
             </div>
         </div>
